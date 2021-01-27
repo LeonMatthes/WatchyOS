31947b054d256b971e289a3a835c2ead21ffd486
Leon Matthes <leon@matthes.biz>
2021-01-27 21:04:47 +0100

    Easier GPIO config

--- src/main/WatchyOS.c
+++ src/main/WatchyOS.c
@@ -8,19 +8,8 @@
 
 void app_main(void)
 {
-  gpio_config_t io_conf;
-  // disable interrupts
-  io_conf.intr_type = GPIO_INTR_DISABLE;
-  //set as output mode
-  io_conf.mode = GPIO_MODE_OUTPUT;
-  //bit mask of the pins that you want to set,e.g.GPIO18/19
-  io_conf.pin_bit_mask = 1ULL << VIB_MOTOR_PIN;
-  //disable pull-down mode
-  io_conf.pull_down_en = 0;
-  //disable pull-up mode
-  io_conf.pull_up_en = 0;
-  //configure GPIO with the given settings
-  gpio_config(&io_conf);
+  gpio_pad_select_gpio(VIB_MOTOR_PIN);
+  gpio_set_direction(VIB_MOTOR_PIN, GPIO_MODE_OUTPUT);
 
   gpio_set_level(VIB_MOTOR_PIN, 1);
 
