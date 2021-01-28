9b0e0ad92a34affbf9d886a71b3a9b16f8945483
Leon Matthes <leon@matthes.biz>
2021-01-28 09:37:14 +0100

    Move from hello_world_main.c to WatchyOS.cpp

--- src/main/WatchyOS.cpp
+++ src/main/WatchyOS.cpp
@@ -4,13 +4,20 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-#include "Arduino.h"
+#include <Arduino.h>
+#include <GxEPD2_BW.h>
 
 #include "constants.h"
 
+extern "C" void app_main(void);
+
+GxEPD2_BW<GxEPD2_154_D67, GxEPD2_154_D67::HEIGHT> display(GxEPD2_154_D67(CS, DC, RESET, BUSY_PIN));
 
 void app_main(void)
 {
+
+  initArduino();
+
   gpio_pad_select_gpio(VIB_MOTOR_GPIO);
   gpio_set_direction(VIB_MOTOR_GPIO, GPIO_MODE_OUTPUT);
 
@@ -20,7 +27,13 @@ void app_main(void)
 
   gpio_set_level(VIB_MOTOR_GPIO, 0);
 
-  initArduino();
+
+  display.init(0, false);
+  display.setFullWindow();
+  display.fillScreen(GxEPD_BLACK);
+  display.drawRect(10, 10, 50, 50, GxEPD_WHITE);
+  display.display(false);
+  display.hibernate();
 
   /* esp_sleep_enable_ext0_wakeup(RTC_PIN, 0); //enable deep sleep wake on RTC interrupt */
   esp_sleep_enable_ext1_wakeup(BTN_PIN_MASK, ESP_EXT1_WAKEUP_ANY_HIGH); //enable deep sleep wake on button press
